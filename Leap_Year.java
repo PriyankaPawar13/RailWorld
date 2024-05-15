0a63fb48c6a33e664b1e4cb00d2daa6bebbcb5a8
PriyankaPawar13 <priyapawar13021@gmail.com>
2024-05-15 23:13:07 +0530

    changes in code

--- Leap_Year.java
+++ Leap_Year.java
@@ -23,21 +23,3 @@ public class Leap_Year {
 	
 
 }
-/*public static void main(String[] args) {
-// TODO Auto-generated method stub
-int year;
-
- 
-  Scanner scn = new Scanner(System.in);
-  year = scn.nextInt();
-
-  if ((year % 400 == 0)
-      || ((year % 4 == 0) && (year % 100 != 0))) {
-
-     
-      System.out.println(year + " : Leap Year");
-  }
-
-  else {
-      
-      System.out.println(year + " : Non - Leap Ye*/
